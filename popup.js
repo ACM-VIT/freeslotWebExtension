9f75929de92cbac160c309b379d61c88e9fc6c41
Shubham Awasthi <aw.shubh@gmail.com>
2019-02-01 13:56:56 +0530

    popup: Created ui

--- popup.js
+++ popup.js
@@ -1,3 +1,18 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {getTT: true});
-}); 
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.sendMessage(tabs[0].id, {getTT: true});
+// }); 
+function failed(){
+    $("#btn").removeClass("loading");
+    $('#btn').html('Failed! Try again?');
+    $('#errormsg').show()
+}
+function loading(){
+    $('#btn').html('Loading...');
+    $('#errormsg').hide()
+    $("#btn").addClass("loading");
+}
+function sucess(){
+    $("#btn").removeClass("loading");
+    $('#btn').html('Get TimeTable');
+    $('#errormsg').hide()
+}
